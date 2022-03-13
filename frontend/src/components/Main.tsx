6a2f21cad96fa9a6c992e2ba0a2f4368de898797
pakkapon <mac@Mas-MacBook-Air.local>
2022-03-13 18:06:42 +0700

    add metamask connection and main component

---
+++ frontend/src/components/Main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Main = () => {
+  // show token value from the wallet
+  // Get the address if different token
+  // Get balance of user in respect to each token
+  // Get information and config from brownie config
+
+  return <div>Main</div>;
+};
