2326096214509147c0de5a8c1abc5d0c830cdf5d
pakkapon <mac@Mas-MacBook-Air.local>
2022-03-15 12:18:37 +0700

    add wallet and staking

--- frontend/src/components/Wallet/Wallet.tsx
+++ frontend/src/components/Wallet/Wallet.tsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Token } from "../Main";
-import { Box } from "@material-ui/core";
+import { Box, Tab, makeStyles } from "@material-ui/core";
+import { TabContext, TabList, TabPanel } from "@material-ui/lab";
+import { StakeForm } from "./StakeForm";
+import { WalletBalance } from "./WalletBalance";
+import { stringify } from "querystring";
 interface WalletProps {
   supportedTokens: Array<Token>;
 }
+const useStyles = makeStyles((theme) => ({
+  tabContent: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: theme.spacing(4),
+  },
+  box: {
+    backgroundColor: "white",
+    borderRadius: "25px",
+  },
+  header: {
+    color: "white",
+  },
+}));
 
 export const Wallet = ({ supportedTokens }: WalletProps) => {
+  const classes = useStyles();
+  const [selectedTokenIndex, setSelectedTokenIndex] = useState<number>(0);
+  const handleTabChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+    setSelectedTokenIndex(parseInt(newValue));
+  };
   return (
     <Box>
-      <h1>Your Wallet</h1>
-      <div>Inside your wallet</div>
+      <h1 className={classes.header}> Your Wallet! </h1>
+      <Box className={classes.box}>
+        <TabContext value={selectedTokenIndex.toString()}>
+          <TabList onChange={handleTabChange} aria-label="stake from tabs">
+            {supportedTokens.map((token, index) => {
+              return (
+                <Tab
+                  label={token.name}
+                  value={index.toString()}
+                  key={index}
+                ></Tab>
+              );
+            })}
+          </TabList>
+          {supportedTokens.map((token, index) => {
+            return (
+              <TabPanel value={index.toString()} key={index}>
+                <div className={classes.tabContent}>
+                  <WalletBalance token={supportedTokens[selectedTokenIndex]} />
+                  <StakeForm token={supportedTokens[selectedTokenIndex]} />
+                </div>
+              </TabPanel>
+            );
+          })}
+        </TabContext>
+      </Box>
     </Box>
   );
 };
