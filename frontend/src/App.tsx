c1886c4f7990ee24989868dec4c718bcefb40a5b
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-03-23 12:40:50 +0700

    add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,37 @@ import { DAppProvider, ChainId } from "@usedapp/core";
 import { Header } from "./components/Header";
 import { Container } from "@material-ui/core";
 import { Main } from "./pages/Main";
-import { Faucets } from "./components/Faucets";
-
+import { Dashboard } from "./pages/Dashboard";
+import { Profile } from "./pages/Profile";
+import { Faucets } from "./pages/Faucets";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 function App() {
   return (
-    <DAppProvider
-      config={{
-        supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337],
-        notifications: {
-          expirationPeriod: 1000,
-          checkInterval: 1000,
-        },
-      }}
-    >
-      <Header />
-      <Faucets></Faucets>
-      <Container maxWidth="md">
-        <Main></Main>
-      </Container>
-    </DAppProvider>
+    <div>
+      <DAppProvider
+        config={{
+          supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337],
+          notifications: {
+            expirationPeriod: 1000,
+            checkInterval: 1000,
+          },
+        }}
+      >
+        <BrowserRouter>
+          <Header />
+          <Container maxWidth="md">
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route index element={<Main />} />
+              <Route path="faucets" element={<Faucets />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="*" element={<Main />} />
+            </Routes>
+          </Container>
+        </BrowserRouter>
+      </DAppProvider>
+    </div>
   );
 }
 
