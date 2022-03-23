19975ade2f46e70d0f832df97c898c988cd3f42a
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-03-23 23:55:21 +0700

    minor style change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,18 @@ import { Dashboard } from "./pages/Dashboard";
 import { Profile } from "./pages/Profile";
 import { Faucets } from "./pages/Faucets";
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  appContainer: {
+    background:
+      "radial-gradient(50% 50% at 50% 50%,#fc077d10 0,rgba(255,255,255,0) 100%)",
+    width: "100%",
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.appContainer}>
       <DAppProvider
         config={{
           supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337],
