a48aefe97f244031829a34d75323988dc5d83406
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-03-23 23:51:27 +0700

    add faucets to faucets page

--- frontend/src/pages/Faucets.tsx
+++ frontend/src/pages/Faucets.tsx
@@ -1,9 +1,52 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { useTransferMacToken } from "../hooks/useTransferMacToken";
 import { Button, CircularProgress } from "@material-ui/core";
 import { formatUnits } from "@ethersproject/units";
 import { useEthers } from "@usedapp/core";
 import { utils } from "ethers";
+import { makeStyles } from "@material-ui/core";
+import MacTokenPng from "../resources/MacToken.png";
+import ETHTokenPng from "../resources/eth.png";
+import DAITokenPng from "../resources/dai.png";
+const useStyles = makeStyles((theme) => ({
+  mainContainer: {
+    fontFamily: "sans-serif",
+  },
+  faucetWrapper: {
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    gap: theme.spacing(1.5),
+    border: "1px solid rgb(229, 232, 235)",
+    borderRadius: theme.spacing(2),
+    background: "rgb(248, 248, 250)",
+  },
+  descContainer: {
+    gap: theme.spacing(2),
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  tokenImg: {
+    width: "36px",
+    // paddingLeft: theme.spacing(2),
+  },
+  amount: {
+    fontWeight: 600,
+    // paddingLeft: theme.spacing(2),
+  },
+  tokenLabel: {
+    fontWeight: 400,
+    // paddingLeft: theme.spacing(2),
+  },
+  heading: {
+    marginTop: 0,
+  },
+}));
+
 export const Faucets = () => {
   const { account, chainId } = useEthers();
 
@@ -12,25 +55,84 @@ export const Faucets = () => {
   const { send: transferMacToken, state: transferMacTokenState } =
     useTransferMacToken();
   const handletransferMacToken = () => {
-    // console.log("Transfering 100 Mac");
-    // console.log(account);
     transferMacToken(amountAsWei.toString(), account);
   };
   const isMining = transferMacTokenState.status === "Mining"; // for check making transaction
-  // useEffect(() => {
-  //   console.log("STATUSSSS", transferMacTokenState);
-  // }, [transferMacTokenState]);
+  const classes = useStyles();
+  const navigate = useNavigate();
   return (
-    <div>
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        onClick={handletransferMacToken}
-        disabled={isMining}
-      >
-        {isMining ? <CircularProgress size={26} /> : `Get Mac coin`}
-      </Button>
+    <div className={classes.mainContainer}>
+      <h1>Faucets</h1>
+      <div className={classes.faucetWrapper}>
+        <h2 className={classes.heading}>MAC Token</h2>
+        <div className={classes.descContainer}>
+          <img
+            className={classes.tokenImg}
+            src={MacTokenPng}
+            alt="token logo"
+          />
+          <span className={classes.amount}>100</span>
+          <span className={classes.tokenLabel}>MAC</span>
+        </div>
+        <Button
+          color="primary"
+          variant="contained"
+          size="large"
+          onClick={handletransferMacToken}
+          disabled={isMining}
+        >
+          {isMining ? <CircularProgress size={26} /> : `Get Mac token`}
+        </Button>
+      </div>
+      <div className={classes.faucetWrapper}>
+        <h2 className={classes.heading}>WETH Token</h2>
+        <div className={classes.descContainer}>
+          <img
+            className={classes.tokenImg}
+            src={ETHTokenPng}
+            alt="token logo"
+          />
+
+          <span className={classes.tokenLabel}>GET WETH</span>
+        </div>
+
+        <Button
+          href="https://faucets.chain.link/"
+          color="primary"
+          variant="contained"
+          size="large"
+        >
+          ETH faucets
+        </Button>
+        <Button
+          href="https://erc20faucet.com/"
+          color="primary"
+          variant="contained"
+          size="large"
+        >
+          WETH Contract
+        </Button>
+      </div>
+      <div className={classes.faucetWrapper}>
+        <h2 className={classes.heading}>DAI Token</h2>
+        <div className={classes.descContainer}>
+          <img
+            className={classes.tokenImg}
+            src={DAITokenPng}
+            alt="token logo"
+          />
+
+          <span className={classes.tokenLabel}>Mint FAU Token (our DAI) </span>
+        </div>
+        <Button
+          href="https://erc20faucet.com/"
+          color="primary"
+          variant="contained"
+          size="large"
+        >
+          erc20faucet.com
+        </Button>
+      </div>
     </div>
   );
 };
